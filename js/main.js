44c941b0e9ebb84cfd58a6a205b31dd80d7e096b
sarah-ga <sarah.hallacher@generalassemb.ly>
2016-08-12 16:05:35 -0400

    unit-8-lesson-2

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Step 1: Write an GET request.
+// We will be getting information on an artist from Spotify's API.
+// The url for the request is https://api.spotify.com/v1/artists/3oDbviiivRWhXwIE8hxkVV
+// The documentation for finding an artist and an example of the response that is returned
+// can be found here: https://developer.spotify.com/web-api/get-artist/
+
+
+// Step 2: On success:
+	// a) Log the response to the console. Open the console and expand the response object to see
+	//    the information it holds. Look for the artist name and the images array.
+	// b) Update the html of the h1 to the name of the artist.
+	// c) Update the src attribute to the url for the first image in the images array.
