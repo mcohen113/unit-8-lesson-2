c8a1f6096a7f1fc215adf66a0ff6f609df326fba
mike cohen <mcohen113@msn.com>
2017-06-04 19:41:54 -0400

    no key

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
 // Step 1: Write an GET request.
-
-SPOT_REQUEST = "https://api.spotify.com/v1/artists/3oDbviiivRWhXwIE8hxkVV/BQBFybZIAB-b04UaH9HXbi81fZ96ZcO-7NPa3VByiqCJULjUzDw-8SvIFmE9kVI0FPRiOHZQTYh-rzcLlsvIA-5xXnV7xNvLbErk_Djzv3A4ub-sHZgkTbwKFPeJyLQK2R-FSUciOA"// We will be getting information on an artist from Spotify's API.
+// We will be getting information on an artist from Spotify's API.
 // The url for the request is https://api.spotify.com/v1/artists/3oDbviiivRWhXwIE8hxkVV
 // The documentation for finding an artist and an example of the response that is returned
 // can be found here: https://developer.spotify.com/web-api/get-artist/
-$($.get(SPOT_REQUEST, function(data) {
- $( ".result" ).html(data);
-  console.log(data); /*optional stuff to do after success */
-});get)
+$.get("https://api.spotify.com/v1/artists/3oDbviiivRWhXwIE8hxkVV", function(data) {
+  /*optional stuff to do after success */
+});
 
 // Step 2: On success:
 	// a) Log the response to the console. Open the console and expand the response object to see
